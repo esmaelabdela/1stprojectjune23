a836aa330d48505b6ec7ee4b8bd49fa1689b3bbf
esmaelabdela <146069460+esmaelabdela@users.noreply.github.com>
2023-12-05 10:38:05 -0600

    1st message

---
+++ src/test/java/variousConcepts/GitTest.java
@@ -0,0 +1,17 @@
+package variousConcepts;
+
+public class GitTest {
+	
+	
+	public static void main(String[] args) {
+		
+	}
+
+	public static void printMe() {
+		
+		System.out.println("Git Test");
+	}
+	
+	
+	
+}
